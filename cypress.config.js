5ac861c6814584ab8bda922e678a8be09d7a4cf1
mhgolestan <golestan1369@gmail.com>
2025-07-31 23:36:45 +0200

    Add `dotenv` and `eslint-plugin-cypress` to `devDependencies` in `package.json`.

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
-
+dotenv = require('dotenv');
+dotenv.config();
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.env.USERNAME = process.env.USERNAME;
+      config.env.PASSWORD = process.env.PASSWORD;
+      return config;
     },
   },
 });
